0a6f04a429f340538c6e4ee0cc88626140de8f13
Weston Carpenter <wc.cc96@gmail.com>
2019-09-25 18:01:53 -0700

    Formatted movie-this output

--- liri.js
+++ liri.js
@@ -12,19 +12,26 @@ var axios = require("axios");
 if (process.argv[2] === 'movie-this') {
     let movieName = process.argv.slice(3).join(" ");
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
     axios.get(queryUrl)
         .then(function (response) {
-            m = response.data
-            console.log('\n', m.Title, '\n',
-                m.Year, '\n',
-                m.Ratings[0].Source + ": " + m.Ratings[0].Value, '\n',
-                m.Ratings[1].Source + ": " + m.Ratings[1].Value, '\n', 
-                m.Country, '\n',
-                m.Language, '\n',
-                m.Plot, '\n',
-                m.Actors,'\n');
+            m = response.data;
+            console.log(`
+Title: ${m.Title}
+Year released: ${m.Year}
+IMDB Rating: ${m.Ratings[0].Value} 
+Rotten Tomatoes Rating: ${m.Ratings[1].Value}  
+Released in: ${m.Country}
+Offered in: ${m.Language}
+Plot summary: ${m.Plot}
+Actors: ${m.Actors}
+            `);
         })
         .catch(function (error) {
             console.log(error)
         });
-    }
+}
+
+if (process.argv[2] === 'concert-this') {
+
+}
